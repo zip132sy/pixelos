6f18a7dd622d30c4ce1a2e5d009c0e99e86a60b6
PixelOS Developer <pixelos@example.com>
2026-06-16 00:09:51 +0800

    fix: 修复 component.list 无网卡时 no such component 错误

--- Installer/OpenOS.lua
+++ Installer/OpenOS.lua
@@ -126,12 +126,20 @@ component.eeprom.set([[
 	
 	print("Starting PixelOS installation...")
 	
-	local internetAddr = component.list("internet")()
-	local internet = internetAddr and component.proxy(internetAddr)
-	if not internet then
+	local internetAddr = component.list("internet")
+	if type(internetAddr) == "function" then
+		internetAddr = internetAddr()
+	elseif type(internetAddr) == "table" then
+		internetAddr = nil
+		for addr in pairs(internetAddr) do internetAddr = addr; break end
+	else
+		internetAddr = nil
+	end
+	if not internetAddr then
 		print("No internet card found")
 		computer.shutdown()
 	end
+	local internet = component.proxy(internetAddr)
 	local response, reason = internet.request("https://gitee.com/zip132sy/pixelos/raw/master/Installer/Main.lua")
 	
 	if response then
